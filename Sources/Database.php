60462d00a3322b071e970355a6edbde4938801be
xhoogland <github@xanland.nl>
2016-05-01 01:03:49 +0200

    Redo "Add some lazy way of forcing singleton in Database" partially
    See commit
    https://github.com/xhoogland/Monique/commit/f937d531cc4b49f635c7474dce1a8770aa987b2b
    for description.

--- Sources/Database.php
+++ Sources/Database.php
@@ -10,11 +10,11 @@
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  *
@@ -25,7 +25,7 @@
  * @version $Id: Database.php 151 2011-08-21 17:43:24Z dik.grapendaal@gmail.com $
  * @package Nuwani
  */
- 
+
 namespace Nuwani;
 
 if (!class_exists ('\PDO'))
@@ -40,29 +40,32 @@ class Database extends \ PDO
          * This property contains the instance of the active MySQLi instance.
          * By utilizing Singleton here we avoid having MySQL connections for
          * every single requests, but rather just when they're needed.
-         * 
+         *
          * @var string
          */
-        
+
         private static $m_sInstance;
-        
+
         /**
          * This property indicates when the current connection has to
          * be killed, and restarted to clear up buffers and all.
-         * 
+         *
          * @var integer
          */
-        
+
         private static $m_nRestartTime;
-        
+
         /**
          * The constructor will connect to the configured MySQL database.
          */
-        
-        public function __construct ()
+
+        public function __construct ($singleton = false)
         {
+                if ($singleton === false)
+                        throw new \Exception("This class should be used as singleton, please use Database::getInstance() instead of new Database()!");
+
                 $aConfiguration = Configuration :: getInstance () -> get ('MySQL');
-                
+
                 parent :: __construct
                 (
                         $aConfiguration ['dsn'],
@@ -70,16 +73,16 @@ class Database extends \ PDO
                         $aConfiguration ['password']
                 );
         }
-        
+
         /**
          * Creates a new connection with the database or returns the active
          * one if there is one, so no double connections for anyone. Returns
          * null if no credentials have been configured, or PDO has been
          * disabled.
-         * 
+         *
          * @return Database|null
          */
-        
+
         public static function getInstance ()
         {
                 if (self :: $m_sInstance == null || self :: $m_nRestartTime < time ())
@@ -88,22 +91,22 @@ class Database extends \ PDO
                         {
                                 self :: $m_sInstance = null;
                         }
-                        
+
                         self :: $m_sInstance = null;
-                        
+
                         $aConfiguration = Configuration :: getInstance () -> get ('MySQL');
-                        
+
                         if (empty ($aConfiguration) ||
                             (isset ($aConfiguration ['enabled']) && $aConfiguration ['enabled'] === false))
                         {
                                 /** No configuration found or PDO is not wanted, bail out. **/
                                 return null;
                         }
-                        
-                        self :: $m_sInstance    = new self ();
+
+                        self :: $m_sInstance    = new self (true);
                         self :: $m_nRestartTime = $aConfiguration ['restart'] + time ();
                 }
-                
+
                 return self :: $m_sInstance;
         }
 };
